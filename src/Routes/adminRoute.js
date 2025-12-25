dd3a587f09ac893a6420475bca15fa95d9f1a7af
SayyadDBvertex <nodedb9@gmail.com>
2025-12-25 18:17:02 +0530

    test

--- src/Routes/adminRoute.js
+++ src/Routes/adminRoute.js
@@ -1,8 +1,8 @@
-import { Router } from "express";
-import { Login } from "../Controller/adminController.js";
+import { Router } from 'express';
+import { Login } from '../Controller/adminController.js';
 
-const adminRouter=Router()
-
-adminRouter.post('/login',Login)
+const adminRouter = Router();
+//admin login route
+adminRouter.post('/login', Login);
 
 export default adminRouter;
